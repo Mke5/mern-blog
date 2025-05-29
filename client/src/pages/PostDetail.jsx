6dc3d0be65bd0170dff7570e15a5eaf78eb50ba0
mke5 <emma08062602618@gmail.com>
2025-05-29 17:26:55 +0100

    Posts component

--- client/src/pages/PostDetail.jsx
+++ client/src/pages/PostDetail.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import PostAuthor from '../components/PostAuthor'
+import { Link } from 'react-router-dom'
+import img from '../images/blog22.jpg'
 
 const PostDetail = () => {
   return (
-    <div>
-      PostDetail
-    </div>
+    <section className="post-detail">
+      <div className="container post-detail__container">
+        <div className="post-detail__header">
+          <PostAuthor />
+
+          <div className="post-detail__buttons">
+            <Link to={`/post/new/edit`} className='btn sm primary'>Edit</Link>
+            <Link to={`/post/new/edit`} className='btn sm danger'>Delete</Link>
+          </div>
+        </div>
+
+        <h1>Post Tilte</h1>
+        <div className="post-detail__thumbnail">
+          <img src={img} alt="" />
+        </div>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur error nobis laborum quibusdam qui cum minus eligendi nesciunt molestiae odit, porro debitis dolorum necessitatibus praesentium aliquid minima officia dignissimos voluptate quia quisquam consequuntur. Expedita nihil numquam eum sequi deleniti officiis consequuntur, quos quaerat voluptatum ducimus qui dicta impedit delectus minima dolor? Molestiae quia ullam rerum.
+        </p>
+      </div>
+    </section>
   )
 }
 
