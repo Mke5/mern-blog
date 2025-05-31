ef73035dd3049d9188c66cd3240782b262f282c2
mke5 <emma08062602618@gmail.com>
2025-05-31 20:09:18 +0100

    register user api

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,48 @@
+const User = require('../models/userModel')
+const HttpError = require('../models/errorModels')
+// const bcrypt = require('bcryptjs')
+
+const bcrypt = ''
+
 // register user controller
 const registerUser = async (req, res, next) => {
-    res.json('register user')
+    try {
+        let {name, email, password, password2} = req.body
+
+        if(!name || !email || !password){
+            return next(new HttpError('Fill in all Fields', 422))
+        }
+
+        email = email.toLowerCase()
+
+        const emailExist = await User.findOne({email: email})
+
+        if(emailExist){
+            return next(new HttpError('Email already exists!', 422))
+        }
+        if((password.trim()).length < 6){
+            return next(new HttpError('Passwords should be at least 6  characters.', 422))
+        }
+        if(password != password2){
+            return next(new HttpError('Passwords do not match.', 422))
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        password = await bcrypt.hash(password, salt)
+
+
+        const newUser = await User.create({name, email, password})
+        res.status(201).json(newUser)
+    } catch (error) {
+        return next(new HttpError('User registration failed.', 422))
+    }
 }
+
+
+
+
+
+
 
 
 // login user
